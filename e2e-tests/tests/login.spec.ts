bfe2c8439a8691f5697a0a1c15c936326e1b7677
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-10-08 09:53:16 +0200

    no e2e test

--- e2e-tests/tests/login.spec.ts
+++ e2e-tests/tests/login.spec.ts
@@ -11,24 +11,24 @@ test.beforeEach(async () => {
     await clearDB();
 });
 
-// test("has a valid user name", async ({page}) => {
-//     const pseudo = "Olga";
-//     const emailToCheck = "olga@gmail.com";
-//     const passwordToCheck = "test@1234";
-//
-//     await User.create({
-//         pseudo: pseudo,
-//         email: emailToCheck,
-//         password: passwordToCheck,
-//     }).save();
-//
-//     await page.goto("http://localhost:3000/auth/login");
-//     await page.getByTestId("login-email").fill(emailToCheck);
-//     await page.getByTestId("login-password").fill(passwordToCheck);
-//     await page.getByTestId("login-button").click();
-//     await page.waitForTimeout(5000);
-//     const disconnectButton = page.getByRole("button", {
-//         name: "Se déconnecter",
-//     });
-//     expect(disconnectButton).toBeVisible();
-// });
+test("has a valid user name", async ({page}) => {
+    const pseudo = "Olga";
+    const emailToCheck = "olga@gmail.com";
+    const passwordToCheck = "test@1234";
+
+    await User.create({
+        pseudo: pseudo,
+        email: emailToCheck,
+        password: passwordToCheck,
+    }).save();
+
+    await page.goto("http://localhost:3000/auth/login");
+    await page.getByTestId("login-email").fill(emailToCheck);
+    await page.getByTestId("login-password").fill(passwordToCheck);
+    await page.getByTestId("login-button").click();
+    await page.waitForTimeout(5000);
+    const disconnectButton = page.getByRole("button", {
+        name: "Se déconnecter",
+    });
+    expect(disconnectButton).toBeVisible();
+});
