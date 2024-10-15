80588cea1f4df72e18f98cca250dad528f3a3769
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-10-15 22:08:27 +0200

    refactor header

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 import { checkUserConnected } from '@/utils/checkConnection'
 import { useRouter } from 'next/router'
@@ -13,19 +12,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { useLogoutLazyQuery } from '@/graphql/generated/schema'
 import { toast } from 'react-toastify'
-
-import {
-    Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarTrigger,
-} from '@/components/ui/menubar'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Navbar() {
-    const [isMenuOpen, setMenuOpen] = useState(false)
     const isConnected = checkUserConnected()
-    const menuRef = useRef<HTMLDivElement>(null)
     const router = useRouter()
     const currentPath = router.pathname
     const [logout] = useLogoutLazyQuery({
@@ -40,35 +31,14 @@ export default function Navbar() {
         },
         fetchPolicy: 'cache-and-network',
     })
-
-    const closeMenu = () => setMenuOpen(false)
-
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (
-                menuRef.current &&
-                !menuRef.current.contains(event.target as Node)
-            ) {
-                closeMenu()
-            }
-        }
-        document.addEventListener('mousedown', handleClickOutside)
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
-        }
-    }, [])
-
     return (
         <div className={`sticky top-0 shadow-lg bg-bgPrimary z-10`}>
             <div className={`flex justify-around md:grid md:grid-cols-3`}>
+                {/*burger menu icon */}
                 <div className={`flex justify-center md:hidden`}>
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
-                            <button
-                                aria-label='Ouvrir ou fermer la navigation'
-                                onClick={() => setMenuOpen(!isMenuOpen)}
-                                // className='md:hidden'
-                            >
+                            <button aria-label='Ouvrir ou fermer la navigation'>
                                 <svg
                                     className='h-6 w-6'
                                     fill='none'
@@ -83,16 +53,14 @@ export default function Navbar() {
                                     />
                                 </svg>
                             </button>
+                            {/*burger menu content*/}
                         </DropdownMenuTrigger>
                         {isConnected ? (
                             <DropdownMenuContent className='w-56'>
                                 <DropdownMenuLabel>Compte</DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuGroup>
-                                    <Link
-                                        href='/mon-profil'
-                                        onClick={closeMenu}
-                                    >
+                                    <Link href='/mon-profil'>
                                         <DropdownMenuItem>
                                             Profil
                                         </DropdownMenuItem>
@@ -104,15 +72,12 @@ export default function Navbar() {
                                 </DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuGroup>
-                                    <Link href='/groupes' onClick={closeMenu}>
+                                    <Link href='/groupes'>
                                         <DropdownMenuItem>
                                             Liste
                                         </DropdownMenuItem>
                                     </Link>
-                                    <Link
-                                        href='/creating-groups'
-                                        onClick={closeMenu}
-                                    >
+                                    <Link href='/creating-groups'>
                                         <DropdownMenuItem>
                                             Créer
                                         </DropdownMenuItem>
@@ -125,7 +90,7 @@ export default function Navbar() {
                             </DropdownMenuContent>
                         ) : (
                             <DropdownMenuContent className='w-56'>
-                                <Link onClick={closeMenu} href='/auth/login'>
+                                <Link href='/auth/login'>
                                     <DropdownMenuItem>
                                         Connexion
                                     </DropdownMenuItem>
@@ -134,7 +99,10 @@ export default function Navbar() {
                         )}
                     </DropdownMenu>
                 </div>
-                <div className={`md:col-start-2 md:flex md:justify-center`}>
+                {/*logo*/}
+                <div
+                    className={`md:flex md:justify-center ${isConnected ? 'md:col-start-1' : 'md:col-start-2'}`}
+                >
                     <Link
                         href='/'
                         className='font-rubik text-2xl text-primaryBlue font-bold flex justify-end items-center md:justify-center md:text-4xl md:tracking-wider'
@@ -148,38 +116,12 @@ export default function Navbar() {
                         <span>Easy Gift</span>
                     </Link>
                 </div>
-                <div className='hidden md:col-start-3 md:flex md:justify-center md:items-center'>
-                    <Menubar>
-                        <MenubarMenu>
-                            <MenubarTrigger>Mon compte</MenubarTrigger>
-                            <MenubarContent>
-                                {isConnected ? (
-                                    <>
-                                        <MenubarItem>
-                                            <Link href='/mon-profil'>
-                                                Mon profil
-                                            </Link>
-                                        </MenubarItem>
-                                        <MenubarItem onClick={() => logout()}>
-                                            déconnexion
-                                        </MenubarItem>
-                                    </>
-                                ) : (
-                                    <MenubarItem>
-                                        <Link href='/auth/login'>
-                                            Connexion
-                                        </Link>
-                                    </MenubarItem>
-                                )}
-                            </MenubarContent>
-                        </MenubarMenu>
-                    </Menubar>
-                </div>
-            </div>
-            {isConnected && (
-                <div className={`grid grid-cols-3 p-3 hidden md:block`}>
-                    <div className='col-start-2 flex justify-evenly'>
-                        <>
+                {/*Navigation*/}
+                <div
+                    className={`hidden ${isConnected ? 'md:col-start-2 md:col-span-3' : 'md:col-start-3'} md:flex md:justify-around md:items-center`}
+                >
+                    {isConnected && (
+                        <nav className='flex justify-around gap-4'>
                             <Link
                                 href='/groupes'
                                 className={`${currentPath === '/groupes' ? 'font-bold' : ''} hover:underline`}
@@ -192,10 +134,49 @@ export default function Navbar() {
                             >
                                 NOUVEL ÉVÉNEMENT
                             </Link>
-                        </>
-                    </div>
+                            <Link
+                                href='/mon-profil'
+                                className={`${currentPath === '/mon-profil' ? 'font-bold' : ''} hover:underline`}
+                            >
+                                MON PROFIL
+                            </Link>
+                        </nav>
+                    )}
+                    {/*connexion/deconnexion icone*/}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant='virgin'>
+                                <Image
+                                    src='/images/icones/connexion.png'
+                                    alt='icone de connexion'
+                                    width={35}
+                                    height={35}
+                                />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        {isConnected ? (
+                            <DropdownMenuContent>
+                                <DropdownMenuItem onClick={() => logout()}>
+                                    Déconnexion
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        ) : (
+                            <DropdownMenuContent>
+                                <Link href='/auth/login'>
+                                    <DropdownMenuItem>
+                                        Connexion
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href='/auth/register'>
+                                    <DropdownMenuItem>
+                                        Inscription
+                                    </DropdownMenuItem>
+                                </Link>
+                            </DropdownMenuContent>
+                        )}
+                    </DropdownMenu>
                 </div>
-            )}
+            </div>
         </div>
     )
 }
