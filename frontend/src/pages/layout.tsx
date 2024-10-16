352a41006916e0ebf08c98b2dbae8b469997a0bd
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-10-16 15:14:47 +0200

    add navbar and footer condition on _app

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,18 +1,23 @@
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const router = useRouter()
+    const hideNavbarRoutes = ['/auth/login', '/auth/register']
+    const hideNavbar = hideNavbarRoutes.includes(router.pathname)
+
     return (
         <div className='w-screen min-h-screen bg-bgPrimary relative flex flex-col justify-between'>
             <div className='w-full max-w-screen-2xl mx-auto'>
-                <Navbar />
+                {!hideNavbar && <Navbar />}
             </div>
             <main className='w-full max-w-screen-2xl mx-auto h-auto flex flex-grow flex-col'>
                 {children}
             </main>
 
-            <Footer />
+            {!hideNavbar && <Footer />}
         </div>
     )
 }
