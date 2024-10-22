39917b87dd7949bf9124c2911b614820a8ff54c6
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-10-22 16:24:38 +0200

    section 1 ok

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -103,11 +103,88 @@ export default function Home() {
             <Head>
                 <title>Page d'accueil - Easy Gift</title>
             </Head>
+            {/*<section*/}
+            {/*    ref={container}*/}
+            {/*    className='my-10 min-h-40 h-auto flex flex-initial flex-wrap content-start items-center mx-auto w-4/5 md:my-28 md:min-h-80 md:justify-evenly md:max-w-2xl lg:flex-nowrap lg:my-44 lg:max-w-7xl xl:my-20 xl:min-h-120  2xl:max-w-[1800px] 2xl:h-[85vh] 2xl:content-center 2xl:my-0'*/}
+            {/*>*/}
+            {/*    <div className='hidden relative w-full order-2 md:block md:max-w-4xl md:min-w-96 lg:mb-24 lg:order-1 lg:max-w-xl:min-h-130 lg:max-w-lg 2xl:max-w-4xl'>*/}
+            {/*        <Image*/}
+            {/*            src='/images/img-pages/hero-img.png'*/}
+            {/*            alt="Photo d'une femme recevant un cadeaux"*/}
+            {/*            width={963}*/}
+            {/*            height={712}*/}
+            {/*            priority={true}*/}
+            {/*            style={{ objectFit: 'cover' }}*/}
+            {/*            className={'image1'}*/}
+            {/*        />*/}
+            {/*    </div>*/}
+            {/*    <div className='h-auto mb-9 sm:max-w-2xl md:w-2/2 md:max-w-3xl lg:mb-0 lg:max-w-lg 2xl:max-w-2xl'>*/}
+            {/*        <h1 className='text1 mb-10 text-4xl md:text-5xl md:mb-14 2xl::text-6xl font-bold text-primaryBlue'>*/}
+            {/*            Easy Gift*/}
+            {/*        </h1>*/}
+            {/*        <p*/}
+            {/*            data-splitting*/}
+            {/*            className='paragraphe1 mb-9 text-lg text-left md:mb-10 md:text-xl 2xl:text-2xl'*/}
+            {/*        >*/}
+            {/*            Prêt à en finir avec les cadeaux ratés ?*/}
+            {/*            <br />*/}
+            {/*            <b>Organisez et échangez</b> avec vos ami(e)s pour*/}
+            {/*            trouver "LA" perle rare sans stress.*/}
+            {/*            <br />*/}
+            {/*            C'est amusant, c'est simple... et c'est un cadeau !*/}
+            {/*        </p>*/}
+            {/*        <Button className='text-base h-9 rounded-md px-3 shadow-lg shadow-slate-400 md:h-11 md:text-lg md:px-8 2xl:h-14 2xl:px-10 2xl:text-2xl'>*/}
+            {/*            {isConnected ? (*/}
+            {/*                <Link href='/creating-groups'>*/}
+            {/*                    Je créé un groupe*/}
+            {/*                </Link>*/}
+            {/*            ) : (*/}
+            {/*                <Link href='/auth/login'>Je me lance</Link>*/}
+            {/*            )}*/}
+            {/*        </Button>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
             <section
                 ref={container}
-                className='my-10 min-h-40 h-auto flex flex-initial flex-wrap content-start items-center mx-auto w-4/5 md:my-28 md:min-h-80 md:justify-evenly md:max-w-2xl lg:flex-nowrap lg:my-44 lg:max-w-7xl xl:my-20 xl:min-h-120  2xl:max-w-[1800px] 2xl:h-[85vh] 2xl:content-center 2xl:my-0'
+                className='flex flex-col lg:flex-row justify-center items-center h-screen'
             >
-                <div className='hidden relative w-full order-2 md:block md:max-w-4xl md:min-w-96 lg:mb-24 lg:order-1 lg:max-w-xl:min-h-130 lg:max-w-lg 2xl:max-w-4xl'>
+                <div className='flex justify-center items-center w-4/5 gap-3'>
+                    <div className='flex flex-col w-11/12 lg:w-4/5 gap-5 lg:gap-6'>
+                        <h1 className='text1 text-3xl font-bold text-primaryBlue lg:text-4xl'>
+                            Easy Gift
+                        </h1>
+                        <div className='paragraphe1 max-w-3xl mx-auto flex flex-col gap-4'>
+                            <p className='text-lg text-gray-700 font-medium lg:text:xl'>
+                                Fini les cadeaux en double et les discussions
+                                secrètes sur WhatsApp !
+                            </p>
+                            <p className='text-base text-gray-700 lg:text-lg'>
+                                Easy Gift est votre espace privé pour coordonner
+                                les cadeaux de Noël en famille ou entre amis.
+                                Chaque membre dispose de son propre fil de
+                                discussion, invisible pour lui mais accessible
+                                aux autres, permettant d'organiser sereinement
+                                les cadeaux tout en préservant l'effet de
+                                surprise.
+                            </p>
+                            <p className='text-base text-gray-700 lg:text-lg'>
+                                Discutez des idées, coordonnez les achats
+                                groupés, et assurez-vous que chacun reçoive des
+                                cadeaux qui lui feront vraiment plaisir - le
+                                tout dans un seul espace organisé et
+                                confidentiel.
+                            </p>
+                        </div>
+                        <div className={'flex justify-end'}>
+                            <Button className={'w-40'}>
+                                <Link href='/creating-groups'>
+                                    Créer un groupe
+                                </Link>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <div className='hidden w-4/5 max-w-3xl mx-auto lg:block'>
                     <Image
                         src='/images/img-pages/hero-img.png'
                         alt="Photo d'une femme recevant un cadeaux"
@@ -117,31 +194,6 @@ export default function Home() {
                         style={{ objectFit: 'cover' }}
                         className={'image1'}
                     />
-                </div>
-                <div className='h-auto mb-9 sm:max-w-2xl md:w-2/2 md:max-w-3xl lg:mb-0 lg:max-w-lg 2xl:max-w-2xl'>
-                    <h1 className='text1 mb-10 text-4xl md:text-5xl md:mb-14 2xl::text-6xl font-bold text-primaryBlue'>
-                        Easy Gift
-                    </h1>
-                    <p
-                        data-splitting
-                        className='paragraphe1 mb-9 text-lg text-left md:mb-10 md:text-xl 2xl:text-2xl'
-                    >
-                        Prêt à en finir avec les cadeaux ratés ?
-                        <br />
-                        <b>Organisez et échangez</b> avec vos ami(e)s pour
-                        trouver "LA" perle rare sans stress.
-                        <br />
-                        C'est amusant, c'est simple... et c'est un cadeau !
-                    </p>
-                    <Button className='text-base h-9 rounded-md px-3 shadow-lg shadow-slate-400 md:h-11 md:text-lg md:px-8 2xl:h-14 2xl:px-10 2xl:text-2xl'>
-                        {isConnected ? (
-                            <Link href='/creating-groups'>
-                                Je créé un groupe
-                            </Link>
-                        ) : (
-                            <Link href='/auth/login'>Je me lance</Link>
-                        )}
-                    </Button>
                 </div>
             </section>
             <section className='w-full mb-28 mx-auto min-h-175 h-full bg-indigo-50 flex flex-col justify-center items-center sm:min-h-160 md:min-h-170  md:mb-14 lg:pt-20 lg:pb-16 lg:flex-wrap lg:flex-row lg:content-center lg:justify-around lg:min-h-140 2xl:pt-0 xl:pb-0 2xl:items-start 2xl:justify-center 2xl:content-end 2xl:flex-row'>
