214ae5b8574aee9204eb2912ecd926a753a8e172
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-10-24 16:59:55 +0200

    index page ok

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -26,7 +26,7 @@ gsap.registerPlugin(useGSAP)
 
 export default function Home() {
     const router = useRouter()
-    const [isConnected, setIsConnected] = useState(checkUserConnected())
+    const isConnected = checkUserConnected()
     const [groups, setGroups] = useState<UserGroupsQuery['userGroups']>([])
 
     const today = new Date()
@@ -53,23 +53,6 @@ export default function Home() {
     }
 
     const { group1, link1, group2, link2, group3, link3 } = FakeDataGroups()
-
-    useEffect(() => {
-        const handleUserChange = () => {
-            const connected = checkUserConnected()
-            setIsConnected(connected)
-            if (!connected) {
-                setGroups([])
-            }
-        }
-
-        window.addEventListener('userChange', handleUserChange)
-
-        // Cleanup on component unmount
-        return () => {
-            window.removeEventListener('userChange', handleUserChange)
-        }
-    }, [])
 
     const handleButtonClick = () => {
         if (isConnected) {
@@ -160,9 +143,9 @@ export default function Home() {
                         </div>
                     </div>
                 </section>
-                <section className='flex flex-col gap-10 w-10/12 justify-center items-center bg-rose-100'>
+                <section className='flex flex-col gap-10 mb-12 w-10/12 justify-center items-center bg-rose-100'>
                     <div className='w-full lg:flex lg:justify-center'>
-                        <h2 className='w-4/5 text-3xl sm:text-center md:text-4xl 4xl:text-5xl font-bold lg:w-full 2xl:mt-32 text-primaryRed'>
+                        <h2 className='w-4/5 text-3xl md:text-4xl 4xl:text-5xl font-bold lg:w-full 2xl:mt-32 text-primaryRed'>
                             Comment ça marche ?
                         </h2>
                     </div>
@@ -247,78 +230,18 @@ export default function Home() {
                             </p>
                         </div>
                     </div>
-                    {/*<article className='w-4/5 sm:max-w-xl 2xl:ml-36 4xl:ml-52 lg:order-3'>*/}
-                    {/*    <p className='mb-8 text-lg md:mb-10 md:text-xl  2xl:text-2xl 2xl:pt-12'>*/}
-                    {/*        Fini les "oups,... tu n'as rien entendu !"*/}
-                    {/*        <br />*/}
-                    {/*        Parce que chez Easy Gift, nous aimons les surprises,*/}
-                    {/*        nous avons créé pour vous le premier espace entièrement*/}
-                    {/*        confidentiel. Ce qui est dit dans une discussion Easy*/}
-                    {/*        Gift reste sur Easy Gift...*/}
-                    {/*        <br />*/}
-                    {/*        L'aventure commence ici :*/}
-                    {/*        <b> Créé un groupe thématique</b> pour chaque occasion.*/}
-                    {/*        <br />*/}
-                    {/*        Fête de fin d'année, anniversaire, baby shower... et{' '}*/}
-                    {/*        <b>*/}
-                    {/*            retrouve instantanément toutes les discussions*/}
-                    {/*            secrètes pour chaque membre*/}
-                    {/*        </b>*/}
-                    {/*        .*/}
-                    {/*    </p>*/}
-                    {/*    <Button*/}
-                    {/*        onClick={handleButtonClick}*/}
-                    {/*        className='text-base h-9 mb-28 rounded-md px-3 shadow-lg shadow-slate-400 md:h-11 md:text-lg md:px-8 lg:mb-8 2xl:h-14 2xl:px-10 2xl:text-2xl'*/}
-                    {/*    >*/}
-                    {/*        {isConnected ? (*/}
-                    {/*            <Link href='/creating-groups'>C'est parti !</Link>*/}
-                    {/*        ) : (*/}
-                    {/*            <Link href='/auth/login'>Ca m'intéresse</Link>*/}
-                    {/*        )}*/}
-                    {/*    </Button>*/}
-                    {/*</article>*/}
-                    {/*<Carousel className='max-w-96 max-h-140 sm:max-w-xl lg:mb-28 lg:ml-16 lg:mr-16 2xl:h-[576px] lg:order-2'>*/}
-                    {/*    <CarouselPrevious className='top-[-25px] left-8 md:top-2/4'>*/}
-                    {/*        Précédent*/}
-                    {/*    </CarouselPrevious>*/}
-                    {/*    <CarouselContent className='max-w-[600px] lg:max-w-[500px] max-h-[700px]'>*/}
-                    {/*        <CarouselItem className='max-w-[700px] max-h-[500px]'>*/}
-                    {/*            <img*/}
-                    {/*                src='/images/img-pages/chat-mobile-mockup.png'*/}
-                    {/*                alt='Exemple de discussion sur un telephone portable'*/}
-                    {/*                className='object-contain w-full h-full'*/}
-                    {/*            />*/}
-                    {/*        </CarouselItem>*/}
-                    {/*        <CarouselItem className='max-w-[700px] max-h-[500px]'>*/}
-                    {/*            <img*/}
-                    {/*                src='/images/img-pages/messenger.png'*/}
-                    {/*                alt="Exemple de discussion sur un écran d'ordinateur"*/}
-                    {/*                className='object-contain w-full h-full'*/}
-                    {/*            />*/}
-                    {/*        </CarouselItem>*/}
-                    {/*        <CarouselItem className='max-w-[700px] max-h-[500px]'>*/}
-                    {/*            <img*/}
-                    {/*                src='/images/img-pages/man-with-a-gift.webp'*/}
-                    {/*                alt='Homme souriant tenant un cadeau dans les mains'*/}
-                    {/*                className='object-contain w-full h-full'*/}
-                    {/*            />*/}
-                    {/*        </CarouselItem>*/}
-                    {/*    </CarouselContent>*/}
-                    {/*    <CarouselNext className='top-[-25px] right-8 md:top-2/4'>*/}
-                    {/*        Suivant*/}
-                    {/*    </CarouselNext>*/}
-                    {/*</Carousel>*/}
                 </section>
-                <section className='w-full h-full flex-grow flex flex-col  gap-6 pb-6 my-10 justify-center items-center md:my-12 2xl:my-28'>
+                <section className='flex flex-col gap-10 mb-12 w-10/12 justify-center items-center bg-rose-100'>
                     {(!isConnected ||
                         (isConnected && groups && groups?.length < 1)) && (
-                        <div className='flex flex-wrap gap-6 justify-center max-w-80 lg:justify-evenly w-10/12 md:max-w-2xl lg:w-full lg:max-w-full'>
-                            <h2 className='w-full flex-grow text-3xl mb-6 text-primaryRed sm:text-center md:mb-10 md:text-4xl 4xl:text-5xl font-bold  2xl:mt-0'>
-                                Retrouve tes groupes
-                            </h2>
-                            <p className='mb-8 text-lg basis-full text-center md:mb-10 md:text-xl  2xl:text-2xl lg:mb-14 '>
-                                Une fois que tu auras créé ou rejoint un groupe,
-                                retrouve-le ici !
+                        <>
+                            <div className='w-full lg:flex lg:justify-center'>
+                                <h2 className='w-4/5 text-3xl md:text-4xl 4xl:text-5xl font-bold lg:w-full 2xl:mt-32 text-primaryRed'>
+                                    Retrouves tes groupes
+                                </h2>
+                            </div>
+                            <p className='w-full lg:flex lg:justify-center text-base text-gray-700 lg:text-lg'>
+                                Voici un exemple d'événements qui ont été créés{' '}
                             </p>
                             <div className='flex flex-wrap gap-6 justify-center max-w-80 lg:justify-center w-10/12 md:max-w-none md:min-w-[680px]  md:w-4/5 lg:w-full lg:max-w-[1000px]'>
                                 <GroupCard
@@ -337,23 +260,26 @@ export default function Home() {
                                     link={link3}
                                 />
                             </div>
-                        </div>
+                        </>
                     )}
 
                     {isConnected && groups && groups.length > 0 && (
-                        <div className='flex flex-wrap gap-6 justify-center max-w-80 lg:justify-evenly w-10/12 md:max-w-2xl lg:max-w-4xl lg:grid lg:grid-cols-3 lg:gap-y-8 lg:gap xl:max-w-[1200px] xl:grid-cols-4 2xl:gap-y-10'>
-                            <h2 className='w-full h-full text-3xl mb-8 text-primaryRed sm:text-center md:mb-10 md:text-4xl lg:col-start-1 lg:col-end-4 xl:col-start-1 xl:col-end-5 font-bold 4xl:text-5xl 2xl:mt-0 2xl:mb-12 align-middle'>
-                                Mes groupes
-                            </h2>
-
-                            {groups.map(group => (
-                                <GroupCard
-                                    key={group.id}
-                                    group={group}
-                                    link={`/group/${group.id}`}
-                                />
-                            ))}
-                        </div>
+                        <>
+                            <div className='w-full lg:flex lg:justify-center'>
+                                <h2 className='w-4/5 text-3xl sm:text-center md:text-4xl 4xl:text-5xl font-bold lg:w-full 2xl:mt-32 text-primaryRed'>
+                                    Mes groupes
+                                </h2>
+                            </div>
+                            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 items-center'>
+                                {groups.map(group => (
+                                    <GroupCard
+                                        key={group.id}
+                                        group={group}
+                                        link={`/group/${group.id}`}
+                                    />
+                                ))}
+                            </div>
+                        </>
                     )}
                 </section>
             </div>
