68c5b345ddf5041c2225601944701e4b6cc1ee41
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-11-06 17:29:38 +0100

    open accordion discussions with url

--- frontend/src/components/discussions/usersMessageAccordion.tsx
+++ frontend/src/components/discussions/usersMessageAccordion.tsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useUserGroupsQuery } from '@/graphql/generated/schema'
 import Link from 'next/link'
 import { Group } from '@/components/GroupCard'
+import { useRouter } from 'next/router'
 
 export default function Accordion() {
-    const [activeIndex, setActiveIndex] = useState<number | null>(null)
+    const router = useRouter()
+    const { groupId, discussionId } = router.query
+
+    const eventId = typeof groupId === 'string' ? parseInt(groupId, 10) : null
+
+    const chatId =
+        typeof discussionId === 'string' ? parseInt(discussionId, 10) : null
+
+    const [activeIndex, setActiveIndex] = useState<number | null>(
+        eventId ?? null
+    )
+
+    useEffect(() => {
+        if (eventId !== null) {
+            setActiveIndex(eventId)
+        }
+    }, [eventId])
+
     const { data, error } = useUserGroupsQuery({
         fetchPolicy: 'cache-and-network',
     })
-
+    console.log('ici', eventId)
     const toggleAccordion = (index: number) => {
         setActiveIndex(activeIndex === index ? null : index)
     }
@@ -32,19 +50,21 @@ export default function Accordion() {
     console.log('accordion', accordions)
     return (
         <div className='w-full max-w-md mx-auto space-y-2'>
-            {accordions?.map((event, index) => (
+            {accordions?.map(event => (
                 <div
-                    key={index}
+                    key={event.id}
                     className='border border-border rounded-lg bg-background'
                 >
                     <button
                         className='w-full px-4 py-3 text-left bg-background hover:bg-border flex justify-between items-center'
-                        onClick={() => toggleAccordion(index)}
+                        onClick={() => toggleAccordion(event.id)}
                     >
-                        <span className='font-medium'>{event.name}</span>
+                        <span className='font-medium'>
+                            {event.id} {event.name}
+                        </span>
                         <svg
                             className={`w-5 h-5 transition-transform duration-200 ${
-                                activeIndex === index ? 'rotate-180' : ''
+                                activeIndex === event.id ? 'rotate-180' : ''
                             }`}
                             fill='none'
                             viewBox='0 0 24 24'
@@ -60,36 +80,39 @@ export default function Accordion() {
                     </button>
                     <div
                         className={`overflow-y-auto transition-all duration-200 ${
-                            activeIndex === index
+                            activeIndex === event.id
                                 ? 'max-h-40 opacity-100'
                                 : 'max-h-0 opacity-0'
                         }`}
                     >
                         {event.discussions.map(item => (
-                            <Link
-                                href={`/group-discussions/${event.id}/discussion/${item.id}`}
-                                className='flex w-full justify-between items-center hover:bg-border'
-                            >
-                                <div className='p-4'>
-                                    Discussion pour {item.discussionName}
-                                </div>
-                                <div className='p-4'>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        strokeWidth={1.5}
-                                        stroke='currentColor'
-                                        className='size-6'
-                                    >
-                                        <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
-                                        />
-                                    </svg>
-                                </div>
-                            </Link>
+                            <div key={item.id}>
+                                <Link
+                                    href={`/group-discussions/${event.id}/discussion/${item.id}`}
+                                    className={`flex w-full justify-between items-center hover:bg-border ${chatId === item.id ? 'bg-border' : ''}`}
+                                >
+                                    <div className='p-4'>
+                                        {item.id} Discussion pour{' '}
+                                        {item.discussionName}
+                                    </div>
+                                    <div className='p-4'>
+                                        <svg
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 24 24'
+                                            strokeWidth={1.5}
+                                            stroke='currentColor'
+                                            className='size-6'
+                                        >
+                                            <path
+                                                strokeLinecap='round'
+                                                strokeLinejoin='round'
+                                                d='M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3'
+                                            />
+                                        </svg>
+                                    </div>
+                                </Link>
+                            </div>
                         ))}
                     </div>
                 </div>
