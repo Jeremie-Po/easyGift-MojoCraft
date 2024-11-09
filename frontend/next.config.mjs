9a148801d48f04c79789eff0a844d59a84df6dee
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-11-09 11:58:26 +0100

    test nextconfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Ajouter output standalone pour Docker
+    output: 'standalone',
     images: {
         domains: ['ucarecdn.com'],
     },
+    // Ajouter la configuration pour les variables d'environnement
+    env: {
+        NEXT_PUBLIC_APOLLO_URI: process.env.NEXT_PUBLIC_APOLLO_URI,
+    },
 }
 
-export default nextConfig
+// Utiliser module.exports au lieu de export default
+module.exports = nextConfig
